15dfaef9dffd0a8cc9972a8de21defcf80439239
RicardoDiasLeite <rica.diasleite@gmail.com>
2022-11-01 18:43:51 +0100

    Ricardo 0.1

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ require('dotenv').config();
 
 var token = process.env.TOKEN
 
-console.log(token.toString())
+console.log(token.toString());
+
+const {REST} = require('@discordjs/rest');
+const { Routes } = require('discord- api-types/v9');
+const { Client, Intents, Collection } = require('discord.js');
+
+const fs = require('fs');
+const path = require('path');
+
+
+const client = new Client({
+    intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS_GUILD_MESSAGES]
+});
+
